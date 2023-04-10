34b31e92b9bf74b941c4f11dc4c0a4005a06efae
Deniz Hürmeydan <denizhurmeydan@gmail.com>
2023-04-11 02:45:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+const num1 =2;
+const num2 =31;
+const multiply= num1*num2;
+console.log(multiply)
+
+
+const random=1
+console.log(Math.floor(Math.random() * 10) + 1);
+//const random = 1
+
+const num3=19;
+const num4=5;
+const mod= num3%num4;
+console.log(mod);
+
+const max=20
+console.log(Math.max(1, 20, 2));
+
+
